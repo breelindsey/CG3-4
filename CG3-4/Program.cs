487ee5c5ec52ab9bacac8608209a879ba03d09c7
breelindsey <35509667+breelindsey@users.noreply.github.com>
2018-07-30 22:20:51 -0500

    Required comment.

--- CG3-4/Program.cs
+++ CG3-4/Program.cs
@@ -6,7 +6,78 @@ namespace CG3_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /* Create a simple calculator that asks the user to input two different numbers and 
+             whether they want to add, subtract, multiple or divide them. 
+             Output the answer back to the console.
+             Be sure to include clear code comments to make your code readable to other programmers and your future self.
+             Upload your program to a new repository called CG 3 - 4.  */
+        
+
+            //In these first two lines, I am declaring my variables. 
+            int firstNumber, secondNumber, answer;
+            string operators;
+
+            //This is a title for my calculator.
+            Console.WriteLine("Bree's Calculator");
+
+            /*Here I am asking the user for the first number to be calculated, parsing that value into an integer,
+            and then telling the program to print the number. */
+            Console.WriteLine("Enter the first number: ");
+            firstNumber = int.Parse(Console.ReadLine());
+
+            //Here I am asking the user which operator they would like to use, and then telling the program to print the operator.
+            Console.WriteLine("Choose an operator: *, -, +, /, or %. ");
+            operators = Console.ReadLine();
+
+            /*Here I am asking the user for the second number to be calculated, parsing that value into an integer,
+            and then telling the program to print the number. */
+            Console.WriteLine("Enter the second number: ");
+            secondNumber = int.Parse(Console.ReadLine());
+
+            /*From here I am giving the program instructions on what to do with the user input.
+            For some reason the closing curly bracket on the first four statements are throwing a code, but not the last. */ 
+            if (operators == "*") ;
+            {
+                answer = firstNumber * secondNumber;
+                Console.WriteLine(answer);
+            }
+             else if (operators == "/") ;
+            {
+                answer = firstNumber / secondNumber;
+                Console.WriteLine(answer);
+            }
+             else if (operators == "+") ;
+            {
+                answer = firstNumber + secondNumber;
+                Console.WriteLine(answer);               
+            }
+             
+            else if (operators == "-") ;
+            {
+                answer = firstNumber - secondNumber;
+                Console.WriteLine(answer);
+            }
+            
+        
+            else if (operators == "%") ;
+            {
+                answer = firstNumber % secondNumber;
+                Console.WriteLine(answer);
+            }
+            Console.ReadLine();
+
+            
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
